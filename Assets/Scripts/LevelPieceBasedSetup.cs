4c9d1584f34682153df66f47083a4e9abca0bf53
Caxin13 <luizalsouza00@gmail.com>
2024-07-09 11:42:18 -0300

    backup bugfixing

---
+++ Assets/Scripts/LevelPieceBasedSetup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class LevelPieceBasedSetup : ScriptableObject
+{
+  [Header ("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+
+    public int piecesNumber = 5;
+    public int piecesStartNumber = 3;
+    public int piecesEndNumber = 1;
+
+
+}
